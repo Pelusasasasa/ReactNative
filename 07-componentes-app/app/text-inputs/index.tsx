dfb4ee88f908d4bc7b3a55628fbed33905d605db
Pelusa <agustinlorenzatto@gmail.com>
2025-07-01 17:59:57 -0300

    Terminando la seccion

--- 07-componentes-app/app/text-inputs/index.tsx
+++ 07-componentes-app/app/text-inputs/index.tsx
@@ -1,10 +1,66 @@
-import { View, Text } from 'react-native';
+import ThemedCard from '@/presentation/shared/ThemedCard';
+import ThemedInputText from '@/presentation/shared/ThemedInputText';
+import ThemedText from '@/presentation/shared/ThemedText';
+import ThemedView from '@/presentation/shared/ThemedView';
+import { useState } from 'react';
+import { KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
 
 const TextInputsScreen = () => {
+
+  const isIos = Platform.OS === 'ios'
+
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    phone: ''
+  })
+
   return (
-    <View>
-      <Text>TextInputsScreen</Text>
-    </View>
+    <KeyboardAvoidingView behavior={isIos ? 'height' : undefined}>
+      <ScrollView>
+        <ThemedView margin>
+          <ThemedCard className='mb-5'>
+            <ThemedInputText 
+              onChangeText={(text) => setForm({...form, name: text})}
+              autoCapitalize='words' 
+              placeholder='Nombre Completo'
+              autoCorrect={false}
+              />
+
+            <ThemedInputText 
+              onChangeText={(text) => setForm({...form, email: text})}
+              keyboardType='email-address'
+              placeholder='Correo Electronico'
+              autoCorrect={false}
+              />
+
+            <ThemedInputText 
+              onChangeText={(text) => setForm({...form, phone: text})}
+              keyboardType='phone-pad'
+              placeholder='Telefono'
+              autoCorrect={false}
+              />
+          </ThemedCard>
+
+          <ThemedCard>
+            <ThemedText>{JSON.stringify(form, null, 2)}</ThemedText>
+          </ThemedCard>
+          <ThemedCard>
+            <ThemedText>{JSON.stringify(form, null, 2)}</ThemedText>
+          </ThemedCard>
+          <ThemedCard>
+            <ThemedText>{JSON.stringify(form, null, 2)}</ThemedText>
+          </ThemedCard>
+          <ThemedCard>
+            <ThemedText>{JSON.stringify(form, null, 2)}</ThemedText>
+          </ThemedCard>
+          <ThemedCard>
+            <ThemedText>{JSON.stringify(form, null, 2)}</ThemedText>
+          </ThemedCard>
+        
+        </ThemedView>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 export default TextInputsScreen;
---
+++ 07-componentes-app/presentation/shared/ThemedInputText.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { TextInputProps } from 'react-native'
+import { TextInput } from 'react-native-gesture-handler'
+interface Props extends TextInputProps {
+    className?: string
+}
+const ThemedInputText = ({className, ...rest}: Props) => {
+  return (
+    <TextInput
+        className='py-4 px-2 text-black dark:text-white'
+        placeholderTextColor="grey"
+        {...rest}
+    />
+  )
+}
+
+export default ThemedInputText
