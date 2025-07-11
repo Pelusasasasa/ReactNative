1f5e03af5f95695b9991003cb2b25078a36d2ba6
Pelusa <agustinlorenzatto@gmail.com>
2025-07-11 17:15:22 -0300

    Empenzando nueva seccion

--- 10-maps-app/app/map/index.tsx
+++ 10-maps-app/app/map/index.tsx
@@ -1,12 +1,32 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import MapView from 'react-native-maps';
 
 const MapScreen = () => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>MapScreen</Text>
+    <View style={styles.container}>
+      <MapView
+      // -30.762091, -57.994706
+      style={styles.map} 
+      initialRegion={{
+        latitude: -30.762091,
+        longitude: -57.994706,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+      />
     </View>
   )
 }
 
-export default MapScreen
+export default MapScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  map: {
+    width: '100%',
+    height: '100%'
+  }
+})
