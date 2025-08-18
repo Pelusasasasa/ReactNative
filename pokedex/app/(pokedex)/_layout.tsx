6cbcdf4e4d172e87318db904d262d65798e6de3f
Pelusa-not <agustinlorenzatto@outlook.com>
2025-08-18 20:44:43 -0300

    First Commit

---
+++ pokedex/app/(pokedex)/_layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import '../../global.css';
+
+const _layout = () => {
+  return (
+    <View className='flex-1 justify-center items-center'>
+      <Text className='text-white text-xl'>_layout</Text>
+    </View>
+  )
+}
+
+export default _layout;
